8d1f53262f97266ba542bf2632ee852cbd685a3a
davycodez <31124918+davycodez@users.noreply.github.com>
2022-04-12 00:04:17 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -29,13 +29,4 @@ char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
 #endif /* DOG_H */
 
-int main(void)
-{
-    struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
