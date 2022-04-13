6e17b42e97515f71ba400f752e28837f164b6449
davycodez <31124918+davycodez@users.noreply.github.com>
2022-04-13 09:44:34 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,24 @@
-#include <stddef.h>
-#ifndef DOG_H
-#define DOG_H
-int _putchar(char c);
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * struct dog - best friend of humankind
- * @name: doggo's name
- * @age: doggo's age
- * @owner: owner's name
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
  */
- struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}; 
+};
+/**
+ * typedef dog_t - defines a new name for type struct dog
+ */
 typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* !DOG_H */
+#endif
